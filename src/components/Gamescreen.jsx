50c9d8b1e7648cb6adf46d61d7d747ae5bbfa538
Michelle Wee <michellew1822@gmail.com>
2024-12-19 12:11:46 -0800

    got navbar to work and also integrated react-router-dom

--- src/components/Gamescreen.jsx
+++ src/components/Gamescreen.jsx
@@ -2,11 +2,10 @@ import React, { useState} from "react";
 /*import Round from "./Gamecomponent.jsx";*/
 import Round from "./Roundbox.jsx";
 import Results from "./Resultsscreen.jsx";
+import Navbar from "./GamescreenElements/Navbar.jsx"
 import "./Gamescreen.css";
 
-
-
-function Gamescreen ({isPressed, setIsPressed}){
+function Gamescreen (){
 
     const [rounds, setRounds] = useState(1);
     const [showFlag, setShowFlag] = useState(true);
@@ -17,32 +16,9 @@ function Gamescreen ({isPressed, setIsPressed}){
 
     return (
         <div>
-            <div className = "navbarContainer">
-                <ul className = "navbarMenu">
-                    <li>
-                        <button className = "instructionsButton">Instructions</button>
-                    </li>
-                    <li>
-                        <button className = "instructionsButton" onClick={() => setIsPressed(false)}> Exit Game </button>
-                    </li>
-                </ul>
-            </div>
-
-            {/*
-                <div className= "instructions">
-                    <h1>Instructions: </h1>
-                    <p>
-                        This is the start of a game. 
-                        Each game has 10 rounds.
-                        In each round, you either win or lose to the computer.
-                        If you win, you get a point. 
-                        If the computer wins, it gets a point.
-                        There will be a point summary at the end of the game, which will determine who wins overall. 
-                    </p>
-                </div>
-                */}
+            <Navbar/>
                     
-                {/* Figure out how to loop this for 10 plays: */}
+            {/* Figure out how to loop this for 10 plays: */}
             <Round
             round = {rounds}/>
 
